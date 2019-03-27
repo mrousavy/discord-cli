672a68c56e4f96aef7177aea73b4f1b372b44976
Marc Rousavy <marcrousavy@hotmail.com>
2019-03-27 09:41:45 +0100

    update interval

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const Discord = require('discord.js');
 const inquirer = require('inquirer');
-const robot = require("robotjs");
 
 const client = new Discord.Client();
 const token = 'NTYwMzYxMjY1MTM5Mjg2MDE3.D3y1uA.jJ4yK5UuGJqK6oKiCHAjAvbWETA';
 const permissions = '68608';
 const clientId = '560361265139286017';
 const clientInviteUrl = `https://discordapp.com/oauth2/authorize?client_id=${clientId}&scope=bot&permissions=${permissions}`;
+
+var channel = null;
+
+var loader = ['/ Connecting..', '| Connecting..', '\\ Connecting..', '- Connecting..'];
+var i = 4;
+var ui = new inquirer.ui.BottomBar({ bottomBar: loader[i % 4] });
+
+var intervalId = setInterval(() => {
+  ui.updateBottomBar(loader[i++ % 4]);
+}, 300);
 
 function selectChannel() {
     var mapped = client.channels.map((v, k) => {
@@ -32,12 +41,12 @@ function selectChannel() {
             }
         ]).then((answers) => {
             var channelIndex = parseInt(answers.index);
-            var channel = mapped[channelIndex];
-            listen(channel);
+            channel = mapped[channelIndex];
+            listen();
         });
 }
 
-function listen(channel) {
+function listen() {
     inquirer
         .prompt([
             {
@@ -57,27 +66,21 @@ function listen(channel) {
 }
 
 client.on('ready', () => {
+    clearInterval(intervalId);
+    ui.updateBottomBar(`Logged in as ${client.user.tag}!`);
     console.log(`Logged in as ${client.user.tag}!`);
     console.log(`Invite link: ${clientInviteUrl}`);
     selectChannel();
 });
 
 client.on('message', msg => {
-    if (msg.member.id != client.user.id) {
+    if (msg.channel.id == channel.id &&
+        msg.member.id != client.user.id) {
         process.stdout.clearLine();
         process.stdout.cursorTo(0);
         process.stdout.write(`${msg.member.displayName}> ${msg.content}\n`);
-        robot.keyTap('space');
+        inquirer.restoreDefaultPrompts();
     }
 });
 
 client.login(token);
-
-// var channels = client.channels;
-// var filteredChannels = client.channels.filter((c) => {
-//     console.log(c.id);
-//     return c.type == "text" && c.id == "farm";
-// });
-// var channel = filteredChannels.first();
-
-
